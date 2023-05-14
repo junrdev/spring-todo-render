bdf21c93f6c87b7d484d3a0117c331c52279899f
star <juniorprogrammer09@gmail.com>
2023-05-14 22:10:50 +0000

    models initial

---
+++ src/main/java/com/springtodo/todoapp/model/Todo.java
@@ -0,0 +1,27 @@
+package com.springtodo.todoapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Todo {
+
+    @SequenceGenerator(name = "todo_seq", allocationSize = 1, sequenceName = "todo_seq")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "todo_seq")
+    private Long Id;
+
+    private String title;
+
+    private String content;
+
+    private Boolean isDone;
+
+}

c98f1cd573bda1f5abcd6bcc9b93086b17076380
star <juniorprogrammer09@gmail.com>
2023-05-14 22:44:59 +0000

    packages scan

--- src/main/java/com/springtodo/todoapp/TodoappApplication.java
+++ src/main/java/com/springtodo/todoapp/TodoappApplication.java
@@ -3,7 +3,9 @@ package com.springtodo.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages= {"com.springtodo.todoapp.*"}
+)
 public class TodoappApplication {
 
 	public static void main(String[] args) {

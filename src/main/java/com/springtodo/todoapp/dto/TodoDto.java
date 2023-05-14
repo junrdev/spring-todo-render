0a2b3da83b6a2405fccafd9a59851f7ff4971bb7
star <juniorprogrammer09@gmail.com>
2023-05-14 22:10:30 +0000

    dtos initial

---
+++ src/main/java/com/springtodo/todoapp/dto/TodoDto.java
@@ -0,0 +1,18 @@
+package com.springtodo.todoapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TodoDto {
+
+    private String title;
+
+    private String content;
+
+}
